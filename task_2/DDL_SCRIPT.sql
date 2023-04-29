ef910c9a3ff0b7a06cd666193d62c8233a9f1c12
MII <igoryha-701@mail.ru>
2023-04-29 23:05:08 +0500

    test_change

--- task_2/DDL_SCRIPT.sql
+++ task_2/DDL_SCRIPT.sql
@@ -46,7 +46,6 @@ CREATE TABLE group (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
 DROP TABLE IF EXISTS day_of_the_week;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
